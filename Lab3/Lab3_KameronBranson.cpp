12b27881c647e0042a5b4bd345b2855085fccd9b
Kameron Branson <kbranson2@student.cscc.edu>
2026-09-20 17:39:44 -0400

    Upload of wip lab3

---
+++ Lab3/Lab3_KameronBranson.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file Lab3_KameronBranson.cpp
+ * @author Kameron Branson
+ * @date 2026-09-20
+ * @brief TUI based area caculator for rectangles and circles
+ */
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	const float PI = 3.14159;
+	float radius;
+	float area;
+	float length;
+	float width;
+	int menu;
+	string areaOfWhat;
+
+	std::cout<<"Menu\n" <<
+		"1. Calculate the Area of a Rectangle\n" <<
+		"2. Calculate the Area of a Circle\n" <<
+		"3. Quit (Hit any other key to quit)\n" <<
+		"Please make a menu selection: ";
+
+	std::cin>>menu;
+
+	if (menu == 1 || menu == 2)
+	{
+		switch(menu)
+		{
+		case 1:
+			std::cout<<"Please enter the length of the rectangle: ";
+			std::cin>>length;
+			std::cout<<"Please enter the width of the rectangle: ";
+			std::cin>>width;
+			area = length * width;
+			areaOfWhat = "rectangle";
+			break;
+
+		case 2:
+			std::cout<<"Please enter the radius of the circle: ";
+			std::cin>>radius;
+			area = PI * (radius * radius);
+			areaOfWhat = "circle";
+			break;
+		}
+
+		std::cout<<"The area of the " << areaOfWhat << " is: " << area;
+
+	}
+
+
+	return 0;
+}
